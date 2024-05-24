012162954481aa37af5004ea310cf1f7421c9c37
sniffjoke <sniffjoke@gmail.com>
2024-05-24 18:57:30 +0200

    add result add history

--- src/app/my-calculator/my-calculator.component.ts
+++ src/app/my-calculator/my-calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 import {KeyValuePipe, NgForOf, NgIf} from "@angular/common";
 
@@ -54,6 +54,7 @@ export class MyCalculatorComponent {
   public history: string[] = []
   public operationsBetweenGroups: CalcOperations[] = []
   public result?: number
+
   public addGroup(): void {
     this.calcGroups.push({
       first: {
@@ -69,10 +70,72 @@ export class MyCalculatorComponent {
 
     this.operationsBetweenGroups.push(CalcOperations.plus)
   }
+
   public removeGroup(index: number): void {
     this.calcGroups.splice(index, 1)
+    this.operationsBetweenGroups.splice(index-1, 1)
   }
 
+  public calcGroup() {
+    let result = 0
 
+    let tempHistory: string[] = []
+
+    this.calcGroups.forEach((group, i) => {
+      if (i === 0) {
+        result = this.calc(
+          this.calcValueWithModif(group.first),
+          this.calcValueWithModif(group.second),
+          group.operations
+        )
+      } else {
+      let tempResult = this.calc(
+        this.calcValueWithModif(group.first),
+        this.calcValueWithModif(group.second),
+        group.operations
+      )
+        result = this.calc(result, tempResult, this.operationsBetweenGroups[i-1])
+      }
+      tempHistory.push(
+        `(
+          ${group.first.modifier !== CalcModifiers.none ? group.first.modifier : ''} ${group.first.value}
+          ${group.operations}
+          ${group.second.modifier !== CalcModifiers.none ? group.second.modifier : ''} ${group.second.value}
+          )`
+      )
+    })
+
+    tempHistory.push(`= ${result}`)
+    this.history.push(tempHistory.join(' '))
+
+    this.result = result
+
+  }
+
+  public calcValueWithModif(value: CalcVar): number {
+    switch (value.modifier) {
+      case CalcModifiers.none:
+        return value.value
+      case CalcModifiers.cos:
+        return Math.cos(value.value)
+      case CalcModifiers.sin:
+        return Math.sin(value.value)
+      case CalcModifiers.square:
+        return Math.pow(value.value, 2)
+    }
+  }
+
+  public calc(first: number, second: number, operation: CalcOperations): number {
+    switch (operation) {
+      case CalcOperations.plus:
+        return first + second
+      case CalcOperations.minus:
+        return first - second
+      case CalcOperations.multiply:
+        return first * second
+      case CalcOperations.divide:
+        return first / second
+    }
+  }
 
 }
