18afe8d813ada2466eb1551793a08d834320292a
sniffjoke <sniffjoke@gmail.com>
2024-05-28 22:47:05 +0200

    add directives @for and ngFor

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -1,14 +1,19 @@
 import {Component} from "@angular/core";
+import {NgFor} from "@angular/common";
 
 
 @Component({
   selector: 'app-directives',
   templateUrl: 'directives.component.html',
-  styleUrls: ['directives.component.scss']
+  styleUrls: ['directives.component.scss'],
+  standalone: true,
+  imports: [NgFor]
 })
 
 export class DirectivesComponent {
 
   public colors: string[] = ['red', 'blue', 'green', 'yellow']
+
+  public isShow = true
 
 }
