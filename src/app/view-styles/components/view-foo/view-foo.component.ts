011eee1ed542cb766af550fe23022660d6c9fad9
sniffjoke <sniffjoke@gmail.com>
2024-07-07 21:42:18 +0200

    add animations

--- src/app/view-styles/components/view-foo/view-foo.component.ts
+++ src/app/view-styles/components/view-foo/view-foo.component.ts
@@ -2,10 +2,31 @@ import {ChangeDetectorRef, Component, ViewEncapsulation} from '@angular/core';
 import {ViewBarComponent} from "../view-bar/view-bar.component";
 import {UserService} from "../../../user.service";
 import {TodoItemComponent} from "../todo-item/todo-item.component";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 export interface TodoItemI {
   text: string;
 }
+
+// const enterTransition = transition(':enter', [
+//   style({opacity: 0}),
+//   animate('2s ease-in', style({opacity: 1})),
+// ])
+
+// const leaveTransition = transition(':leave', [
+//   style({opacity: 1}),
+//   animate('1s ease-in', style({opacity: 0})),
+// ])
+
+// const fadeIn = trigger('fadeIn', [enterTransition])
+// const fadeOut = trigger('fadeOut', [leaveTransition])
+
+const fadeInOut = trigger('fadeInOut', [
+  state('open', style({opacity: 1})),
+  state('close', style({opacity: 0})),
+  transition('open => *', [animate('1s ease-out')]),
+  transition('close => open', [animate('1s ease-in')])
+])
 
 @Component({
   selector: 'app-view-foo',
@@ -19,9 +40,13 @@ export interface TodoItemI {
   styles: [
     'h1 {color: green}'
   ],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  // animations: [fadeIn, fadeOut]
+  animations: [fadeInOut]
 })
 export class ViewFooComponent {
+
+  public isShow = false
 
   public todoArr: TodoItemI[] = [
     {
@@ -35,7 +60,8 @@ export class ViewFooComponent {
     }
   ]
 
-  constructor(private _userService: UserService, private _cdr: ChangeDetectorRef) {}
+  constructor(private _userService: UserService, private _cdr: ChangeDetectorRef) {
+  }
 
   public login(): void {
     this._userService.login()
@@ -48,6 +74,14 @@ export class ViewFooComponent {
 
   public changeStream(): void {
     this._userService.StringSubject$.next('Two')
+  }
+
+  public onAnimationStart(event: any) {
+    console.log('Start', event)
+  }
+
+  public onAnimationDone(event: any) {
+    console.log('Done', event)
   }
 
 }
